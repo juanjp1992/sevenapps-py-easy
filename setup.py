d8fb21634e721c3a4992deb75a730c6b2d39e37b
Juan Jimenez Perez <juan.jimenez@zitrogames.com>
2024-05-24 00:17:12 +0200

    0.0.1: first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'sevenapps-py-easy',
+    version= '0.0.1',
+    license= 'MIT',
+    description= "Paquete creado para optimizar mi trabajo con python unificando lo más necesitado",
+    author= "SevenApps Studio",
+    packages=find_packages(),
+    url= 'https://github.com/escribanoVegaJose/pypipackageexample'
+)
