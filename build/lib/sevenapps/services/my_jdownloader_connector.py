2babd9abefe5e6d07f1dde9222113e1fbaeca626
JuanJP <juanjperez1992@gmail.com>
2025-04-12 17:34:40 +0200

    Added chatgpt class connector and easy upload scripts

--- build/lib/sevenapps/services/my_jdownloader_connector.py
+++ build/lib/sevenapps/services/my_jdownloader_connector.py
@@ -4,16 +4,69 @@ import myjdapi
 class MyJDownloaderConnector:
 
     def __init__(self, user, password, device, app_key):
-        self.jdownloader_connection = self.connect_to_jdownloader(user, password, device, app_key)
+        self.user = user
+        self.password = password
+        self.device = device
+        self.app_key = app_key
+        self.jd_connection = None
+        self.device_session = None
+        self.connect_to_jdownloader()
 
-    def connect_to_jdownloader(self, user, password, device, app_key):
+    # CONEXION A JDOWLOADER ##########################
 
-        jd = myjdapi.Myjdapi()
-        jd.set_app_key(app_key)
-        jd.connect(user, password)
+    def connect_to_jdownloader(self):
+        self.jd_connection = myjdapi.Myjdapi()
+        self.jd_connection.set_app_key(self.app_key)
+        self.jd_connection.connect(self.user, self.password)
 
         # Obtener los dispositivos disponibles
-        jd.update_devices()
+        self.jd_connection.update_devices()
 
-        # Retornamos el device
-        return jd.get_device(device)
+        # Obtenemos los datos del dispositivo especifico
+        self.device_session = self.jd_connection.get_device(self.device)
+        
+    
+    def reconnect_if_needed(self):
+        # Verifica si la conexión está activa
+        if not self.jd_connection.is_connected():
+            print("Reconectando a JDownloader...")
+            self.connect_to_jdownloader()
+    
+    # AÑADIR ENLACES ##########################
+
+    # Añadir el enlace al LinkGrabber
+    def add_link_to_linkgrabber(self, link):
+        self.reconnect_if_needed()
+        self.device_session.linkgrabber.add_links([link])
+        print(f"Enlace añadido a LinkGrabber: {link}")
+
+
+    # COMPROBAR ENLACES ##########################
+
+    # Retornamos los enlaces de la pestaña downloads para ver que se hayan subido
+    def check_download_links(self):
+        self.reconnect_if_needed()
+        return self.device_session.downloads.query_links()
+        
+    # Retornamos los enlaces de la pestaña downloads para ver que se hayan subido
+    def check_linkgrabber_links(self):
+        self.reconnect_if_needed()
+        return self.device_session.linkgrabber.query_links()
+    
+    # LIMPIEZA DE ENLACES ##########################
+
+    # Limpiar la pestaña downloads
+    def cleanup_download_links(self):
+        self.reconnect_if_needed()
+        self.device_session.downloads.cleanup("DELETE_ALL", "REMOVE_LINKS_AND_RECYCLE_FILES", "ALL")
+    
+    # Limpiar el LinkGrabber
+    def cleanup_linkgrabber(self):
+        self.reconnect_if_needed()
+        self.device_session.linkgrabber.cleanup("DELETE_ALL", "REMOVE_LINKS_AND_DELETE_FILES", "ALL")
+        
+    def full_cleanup(self):
+        self.cleanup_download_links()
+        self.cleanup_linkgrabber()
+
+    
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+
+# Leer el contenido del README
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='sevenapps-py-easy',
-    version='0.0.13',
+    version='0.0.14',
     license='MIT',
     description="Paquete creado para optimizar mi trabajo con python unificando lo más necesitado",
     author="SevenApps Studio",
